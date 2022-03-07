24fbd93dc4925b3717480422ecb192431bf43b6d
Robin Wilson <robin@rtwilson.com>
2022-03-07 19:30:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='offline_folium',
     version='0.1',
-    url='https://github.com/mypackage.git',
+    url='https://github.com/debrief/offline_folium',
     author='Robin Wilson',
     author_email='robin@rtwilson.com',
-    description='Description of my package',
-    packages=find_packages('src'),    
+    description='Allows using folium with no internet connection',
+    packages=find_packages('.'),    
     install_requires=['folium'],
 )
